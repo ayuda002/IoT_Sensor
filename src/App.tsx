81eb4250028bf379bb850fed7fc3fffec2dd689b
ayuda002 <yudai02@puc.pu-toyama.ac.jp>
2023-07-12 17:30:25 +0900

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback, memo } from "react";
 import "./App.css";
 import SensorData from "./sensor.json";
 
-export const Rolette = memo((props: { tags: any,kakutei:boolean }) => {
+export const Rolette = memo((props: { tags: any; kakutei: boolean }) => {
   const [start, setStart] = useState(false);
   const [index, setIndex] = useState(0);
 
@@ -10,39 +10,32 @@ export const Rolette = memo((props: { tags: any,kakutei:boolean }) => {
 
   const [ran, setRan] = useState(random_data);
 
-  const sensro_tags = props.tags
+  const sensro_tags = props.tags;
 
   const startRoulette = useCallback(() => {
     setStart(!start);
   }, [start]);
 
-  useEffect(()=>{
-    if(props.kakutei){
-      console.log(props.kakutei)
-      
-    }
-  },[props.kakutei])
-
   //ルーレットを回す処理
   useEffect(() => {
     const sensor_check = () => {
-      SensorData.filter(function(item){
-        for(let i=0;i<sensro_tags.length;i++){
-          if(item.id === sensro_tags[i]){
+      SensorData.filter(function (item) {
+        for (let i = 0; i < sensro_tags.length; i++) {
+          if (item.id === sensro_tags[i]) {
             random_data.push(item.url);
           }
         }
-      })
+      });
       setRan(random_data);
     };
     sensor_check();
     if (start) {
       const interval = setInterval(() => {
         setIndex((oldIndex) => {
-          if (oldIndex < random_data.length - 1) return oldIndex+1;
+          if (oldIndex < random_data.length - 1) return oldIndex + 1;
           return 0;
         });
-      }, 1); //ルーレットの中身を切り替える速度
+      }, 100); //ルーレットの中身を切り替える速度
       return () => clearInterval(interval);
     } else if (!start) {
     }
@@ -51,10 +44,21 @@ export const Rolette = memo((props: { tags: any,kakutei:boolean }) => {
   return (
     <>
       <div>
-        {ran.length > 0 ?
-        <div className="box has-background-info"><img style={{height:"430px",width:"280px"}} src={ran[index]}></img></div>
-        : <div className="box has-background-info"><img style={{height:"430px",width:"280px"}} src="https://pics.prcm.jp/87c6b6fc41d13/79654551/png/79654551_220x220.png"></img></div>
-        }
+        {ran.length > 0 ? (
+          <div className="box has-background-info">
+            <img
+              style={{ height: "430px", width: "280px" }}
+              src={ran[index]}
+            ></img>
+          </div>
+        ) : (
+          <div className="box has-background-info">
+            <img
+              style={{ height: "430px", width: "280px" }}
+              src="https://pics.prcm.jp/87c6b6fc41d13/79654551/png/79654551_220x220.png"
+            ></img>
+          </div>
+        )}
       </div>
       <button
         className="button is-fullwidth btn btn--orange btn--cubic btn--shadow mt-3"
@@ -68,11 +72,12 @@ export const Rolette = memo((props: { tags: any,kakutei:boolean }) => {
 });
 
 function App() {
-
-  var random_data: string[] = new Array(0);
-  const [ran, setRan] = useState(random_data);
-  const [kakutei,setKakutei]=useState(false);
-  const [checkedTags, setCheckedTags] = useState<number[]>([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]);
+  let random_data: string[] = new Array(0);
+  const [kakutei, setKakutei] = useState(false);
+  const [checkedTags, setCheckedTags] = useState<number[]>([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
+  ]);
   const handleCheckTag = useCallback(
     (tagId: number) => {
       if (checkedTags.includes(tagId)) {
@@ -91,6 +96,17 @@ function App() {
     [checkedTags]
   );
 
+  useEffect(() => {
+    const img = new Image();
+    const img2 = new Image();
+    const img3 = new Image();
+    img.src =
+      "http://tpuccm.sakura.ne.jp/millezo/Iot/sensor/Lightning_sensor.png";
+    img2.src = "http://tpuccm.sakura.ne.jp/millezo/Iot/sensor/ICcard.png";
+    img3.src =
+      "http://tpuccm.sakura.ne.jp/millezo/Iot/sensor/Soilhumidity_sensor.png";
+  }, []);
+
   return (
     <div className="container m-5">
       <div className="columns">
@@ -100,7 +116,6 @@ function App() {
               <p className="card-header-title has-background-info has-text-white">
                 Iot Sensor
               </p>
-              <button onClick={()=>{setKakutei(!kakutei)}}>確定</button>
             </header>
             <div className="card-content iotscroll">
               <div className="content">
@@ -136,13 +151,13 @@ function App() {
               <div className="content display_center">
                 <div className="columns">
                   <div className="column">
-                    <Rolette tags={checkedTags} kakutei={kakutei}/>
+                    <Rolette tags={checkedTags} kakutei={kakutei} />
                   </div>
                   <div className="column">
-                    <Rolette tags={checkedTags} kakutei={kakutei}/>
+                    <Rolette tags={checkedTags} kakutei={kakutei} />
                   </div>
                   <div className="column">
-                    <Rolette tags={checkedTags} kakutei={kakutei}/>
+                    <Rolette tags={checkedTags} kakutei={kakutei} />
                   </div>
                 </div>
               </div>
